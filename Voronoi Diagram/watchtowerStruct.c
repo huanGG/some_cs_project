e9d8bffb5e94827e9c3f6dde1e8c107b72bda0c5
huanGG <changhuan1993@gmail.com>
2022-05-22 21:42:38 +0800

    first commit, add voronoi diagram assignment

---
+++ Voronoi Diagram/watchtowerStruct.c
@@ -0,0 +1,11 @@
+#include "watchtowerStruct.h"
+struct watchtowerStruct
+{
+    char *watchtowerID;
+    char *postcode;
+    int populationServed;
+    char *contact;
+    double x;
+    double y;
+    int face;
+};
